1057e7164fe3195eb8c9b9f4baf7490deefba3d9
Manya Kalra <kalramanya212@gmail.com>
2026-08-15 23:06:30 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider proxyUrl="/__clerk">
       <html lang="en">
         <body>{children}</body>
       </html>
